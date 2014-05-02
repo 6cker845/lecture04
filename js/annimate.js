760565cf795a2d4f1137c6aa0a4daf629120332b
6cker845 <nick_fun6king@yahoo.co.jp>
2014-05-02 17:33:34 +0900

    動作の解説を追加
    jsアニメーションの解説

--- js/annimate.js
+++ js/annimate.js
@@ -1,6 +1,7 @@
 var endFlipFoxkeh = function(){
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
 	foxkeh.removeEventListener("animationend", endFlipFoxkeh);
+	//animationのイベントを取り除くことで初期状態に戻す。
 
 	foxkeh.setAttribute("class", "");
 };
@@ -10,16 +11,22 @@ var endSlideinFoxkehAndStartFlipFoxkeh = function(){
 
 	foxkeh.removeEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh);
 	foxkeh.addEventListener("animationend", endFlipFoxkeh);
+	//removeEventListenerを一回記述することで"animation”を再定義している。
+	//endFlipFoxkehを呼び出す。
 
 	foxkeh.setAttribute("class", "flip");
+	//foxkehの振る舞いはflip.cssに記述してあるのでそれを呼び出す。
 };
 
 var startSlideinFoxkeh = function(){
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
 	foxkeh.setAttribute("class", "slidein");
+	//slideinをクラスに設定。foxkehの振る舞いはslidein.cssに記述してあるのでそれを呼び出す。
 
 	foxkeh.addEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh);
+	//ここでendSlideinFoxkehAndStartFlipFoxkehを呼び出している。
 };
 
 var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
 slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
+//ボタンをclickすることでstartSlideinFoxeh関数が呼び出される。
--- js/slidein.js
+++ js/slidein.js
@@ -4,19 +4,16 @@ var endSlideinFoxkeh = function(){
 	foxkeh.setAttribute("class", "slidein");
 };
 
-var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
-slideinFoxkehButton.addEventListener("mouseover", slideinFoxkeh);
-=======
-	foxkeh.setAttribute("class", "");
-};
-
 var startSlideinFoxkeh = function(){
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
 	foxkeh.setAttribute("class", "slidein");
+	//slideinをクラスに設定。slideinの振る舞いはcssに記述してある。アニメーションが始まる。
 
 	foxkeh.addEventListener("animationend", endSlideinFoxkeh);
+	//
 };
 
 var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
 slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
 >>>>>>> origin/make_slidein_retriable
+//ボタンを押すことでstartSlideinFoxeh関数が呼び出される。
